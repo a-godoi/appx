9b16d22a4cbc37d005a45f3a78551ba18bd76b10
Alécio A P Godoi <alecio.godoi@usp.br>
2024-09-07 16:33:07 -0300

    Alteração Coveralls - 6

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,11 +17,22 @@
 require 'capybara/rspec'
 require 'simplecov'
 require 'coveralls'
+#
 Coveralls.wear!
 SimpleCov.formatters = [
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter ]
-SimpleCov.start
+SimpleCov.start 'rails' do
+  if ENV['CI']
+    require 'simplecov-lcov'
+    SimpleCov::Formatter::LcovFormatter.config do |c|
+      c.report_with_single_file = true
+      c.single_report_path = 'coverage/lcov.info'
+    end
+    formatter SimpleCov::Formatter::LcovFormatter
+  end
+  add_filter %w[version.rb initializer.rb]
+end
 #
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
